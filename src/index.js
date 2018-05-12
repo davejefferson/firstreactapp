a55ac72ca62d37339461b8e1d61d4205d559ccc3
davejefferson <36189939+davejefferson@users.noreply.github.com>
2018-05-12 17:24:56 +0100

    created hello component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Hello from './Hello.js';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<h1>Hello World </h1>, document.getElementById('root'));
+ReactDOM.render(<Hello />, document.getElementById('root'));
 registerServiceWorker();

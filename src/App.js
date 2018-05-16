df9eda304886d0c7e5dc91a743e528658470aa40
davejefferson <36189939+davejefferson@users.noreply.github.com>
2018-05-16 19:41:09 +0100

    working search component, style complete

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, {Component} from "react";
+import CardList from "./CardList";
+import { robots } from "./robots";
+import SearchBox from "./SearchBox";
+import "./app.css";
+
+ class App extends Component {
+     constructor(){
+         super();
+         this.state= {
+             robots: robots,
+             searchfield: ""
+         }
+     }
+
+     onsSearchChange = (event) => {
+        this.setState({searchfield: event.target.value})
+        
+     }
+     render() {
+         const filteredRobots = this.state.robots.filter(robots => {
+             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+         })
+         return (
+             <div className="tc">
+                 <h1 className="f1">RoboFriends</h1>
+                 <SearchBox searchChange={this.onsSearchChange} />
+                 <CardList robots={filteredRobots} />
+             </div>
+         );
+     }
+    
+}
+
+export default App;

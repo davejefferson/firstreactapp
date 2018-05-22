812beffea96a3c5a76d9cd8eebe3a923406db75d
davejefferson <36189939+davejefferson@users.noreply.github.com>
2018-05-22 12:34:25 +0100

    cleanup app.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -25,13 +25,13 @@ import "./app.css";
         
      }
      render() {
-         const filteredRobots = this.state.robots.filter(robots => {
-             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+         const {robots, searchfield} = this.state;
+         const filteredRobots = robots.filter(robot => {
+             return robot.name.toLowerCase().includes(searchfield.toLowerCase());
          })
-         if (this.state.robots.length === 0) {
-             return <h1>Loading</h1>
-         } else{
-             return (
+         return !robots.length ?
+             <h1>Loading</h1> :
+         (
                  <div className="tc">
                      <h1 className="f1">RoboFriends</h1>
                      <SearchBox searchChange={this.onsSearchChange} />
@@ -40,7 +40,6 @@ import "./app.css";
                         </Scroll>
                  </div>
              );
-         }
          
      }
     

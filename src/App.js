4e24281c97fad8caef9aea59fce33b9cc356f361
davejefferson <36189939+davejefferson@users.noreply.github.com>
2018-05-22 12:00:12 +0100

    added jsonplaceholder api

--- src/App.js
+++ src/App.js
@@ -8,9 +8,15 @@ import "./app.css";
      constructor(){
          super();
          this.state= {
-             robots: robots,
+             robots: [],
              searchfield: ""
          }
+     }
+
+     componentDidMount()  {
+         fetch("https://jsonplaceholder.typicode.com/users")
+         .then(response => response.json())
+         .then(users => this.setState({robots: users})); 
      }
 
      onsSearchChange = (event) => {
@@ -21,13 +27,18 @@ import "./app.css";
          const filteredRobots = this.state.robots.filter(robots => {
              return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
          })
-         return (
-             <div className="tc">
-                 <h1 className="f1">RoboFriends</h1>
-                 <SearchBox searchChange={this.onsSearchChange} />
-                 <CardList robots={filteredRobots} />
-             </div>
-         );
+         if (this.state.robots.length === 0) {
+             return <h1>Loading</h1>
+         } else{
+             return (
+                 <div className="tc">
+                     <h1 className="f1">RoboFriends</h1>
+                     <SearchBox searchChange={this.onsSearchChange} />
+                     <CardList robots={filteredRobots} />
+                 </div>
+             );
+         }
+         
      }
     
 }

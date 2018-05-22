a9ff6aae481aca91474f70c648a43099e7f3c2d8
davejefferson <36189939+davejefferson@users.noreply.github.com>
2018-05-22 14:41:15 +0100

    installed and connected redux-logger

--- src/index.js
+++ src/index.js
@@ -4,15 +4,18 @@ import ReactDOM from 'react-dom';
 // import provider to send store to containers, import connect to access the store in containers 
 import {Provider} from "react-redux";
 
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import { createLogger } from "redux-logger";
 import './index.css';
 import App from "./containers/App";
 import registerServiceWorker from './registerServiceWorker';
 import {searchRobots} from "./reducers";
 import "tachyons";
 
+const logger= createLogger();
+
 // Create Store for redux
-const store = createStore(searchRobots)
+const store = createStore(searchRobots, applyMiddleware(logger));
 
 
 ReactDOM.render(
